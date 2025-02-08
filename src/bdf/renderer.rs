324508f82ab20cdffed008705c17e18fcd944d8c
mani3 <kazuya.4da@gmail.com>
2025-02-08 23:48:36 +0900

    :zap: update command for text horizontal

--- src/bdf/renderer.rs
+++ src/bdf/renderer.rs
@@ -1,13 +1,29 @@
 
-pub fn render_bitmap(bitmap: &Vec<String>, pixel: &str, space: &str) -> String {
+pub fn render_bitmap(bitmap: &[String], pixel: &str, space: &str) -> String {
     let mut result = String::new();
-    for hex_line in bitmap {
-        if let Ok(num) = u32::from_str_radix(hex_line, 16) {
-            let bin_str = format!("{:0width$b}", num, width = hex_line.len() * 4);
-            let dot_line = bin_str.replace('0', space).replace('1', pixel);
-            result.push_str(&dot_line);
-            result.push('\n');
+    for bin_str in bitmap {
+        let dot_line = bin_str.replace('0', space).replace('1', pixel);
+        result.push_str(&dot_line);
+        result.push('\n');
+    }
+    result
+}
+
+
+pub fn concat_bitmaps(bitmaps: Vec<&Vec<String>>) -> Vec<String> {
+    let height = bitmaps.iter().map(|bmp| bmp.len()).max().unwrap_or(0);
+    let mut result = vec![String::new(); height];
+
+    for bitmap in bitmaps {
+        for (y, hex_line) in bitmap.iter().enumerate() {
+            let bin_str = format!(
+                "{:0width$b}",
+                u32::from_str_radix(hex_line, 16).unwrap_or(0),
+                width = hex_line.len() * 4
+            );
+            result[y].push_str(&bin_str);
         }
     }
+    // for line in result
     result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,56 @@
+use rust_embed::RustEmbed;
 use std::env;
 use std::fs::File;
-use std::io::{self, BufReader};
+use std::io::{self, BufReader, Cursor};
 
-use bdf_parser::bdf::{parser::BdfParser, renderer::render_bitmap};
+use bdf_parser::bdf::{
+    parser::BdfParser,
+    renderer::{concat_bitmaps, render_bitmap},
+};
+
+#[derive(RustEmbed)]
+#[folder = "fonts/"]
+struct Assets;
 
 fn print_usage(program_name: &str) {
-    eprintln!("Usage: {} <BDF file> <text>", program_name);
-}
-
-fn process_character(bdf: &BdfParser, c: char, pixel: &str, space: &str) {
-    let code = c as u32;
-
-    if let Some(bitmap) = bdf.get_bitmap(code) {
-        println!("Bitmap for character {}: {:?}", code, bitmap);
-        println!("{}", render_bitmap(bitmap, pixel, space));
-    } else {
-        eprintln!("Character '{}' (code: {}) not found in BDF.", c, code);
-    }
+    eprintln!("Usage: {} <text> [pixel] [space]", program_name);
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
+    if args.len() < 2 {
         print_usage(&args[0]);
         std::process::exit(1);
     }
 
-    let file_path = &args[1];
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
+    if let Some(font) = Assets::get("misaki_gothic.bdf") {
+        let cursor = Cursor::new(font.data.into_owned());
+        let reader = BufReader::new(cursor);
+        let bdf = BdfParser::parse(reader)?;
 
-    let bdf = BdfParser::parse(reader)?;
+        let pixel = args.get(2).and_then(|s| s.chars().next()).unwrap_or('＠');
+        let space = args.get(3).and_then(|s| s.chars().next()).unwrap_or('　');
+        let mut bitmaps = Vec::new();
 
-    println!("BDF Version: {}", bdf.version);
-    println!("Font Name: {}", bdf.font);
-    println!("Character Count: {}", bdf.count);
+        for c in args[1].chars() {
+            if let Some(bitmap) = bdf.get_bitmap(c as u32) {
+                bitmaps.push(bitmap);
+            } else {
+                eprintln!("Warning: Character '{}' not found in BDF.", c);
+            }
+        }
 
-    let pixel = "＠"; // Character to display for 1
-    let space = "　"; // Character to display for 0
+        if bitmaps.is_empty() {
+            eprintln!("No valid characters found.");
+            std::process::exit(1);
+        }
 
-    for c in args[2].chars() {
-        process_character(&bdf, c, pixel, space);
+        let concatenated_bitmap = concat_bitmaps(bitmaps);
+        let text = render_bitmap(&concatenated_bitmap, &pixel.to_string(), &space.to_string());
+        println!("{}", text);
+    } else {
+        eprintln!("BDF file not found");
+        std::process::exit(1);
     }
 
     Ok(())
