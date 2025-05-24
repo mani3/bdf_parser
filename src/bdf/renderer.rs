897f2b903a58269507d6013ea51879ed4367ea6c
mani3 <kazuya.4da@gmail.com>
2025-05-24 22:11:42 +0900

    :hammer: add padding

--- src/bdf/renderer.rs
+++ src/bdf/renderer.rs
@@ -15,13 +15,27 @@ pub fn concat_bitmaps(bitmaps: Vec<&Vec<String>>) -> Vec<String> {
     let mut result = vec![String::new(); height];
 
     for bitmap in bitmaps {
-        for (y, hex_line) in bitmap.iter().enumerate() {
+        let bmp_height = bitmap.len();
+        let pad_top = (height - bmp_height) / 2;
+
+        // 上パディング
+        for line in result.iter_mut().take(pad_top) {
+            line.push_str(&"0".repeat(bitmap[0].len() * 4));
+        }
+
+        // ビットマップ本体
+        for (i, hex_line) in bitmap.iter().enumerate() {
             let bin_str = format!(
                 "{:0width$b}",
                 u32::from_str_radix(hex_line, 16).unwrap_or(0),
                 width = hex_line.len() * 4
             );
-            result[y].push_str(&bin_str);
+            result[pad_top + i].push_str(&bin_str);
+        }
+
+        // 下パディング
+        for line in result.iter_mut().skip(pad_top + bmp_height) {
+            line.push_str(&"0".repeat(bitmap[0].len() * 4));
         }
     }
     // for line in result
